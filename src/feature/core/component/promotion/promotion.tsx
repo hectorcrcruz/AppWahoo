97b516678e368bf3d894f48c6c5316cdb0c01b4f
CristianDev <camilovargas_12@misena.edu.co>
2025-08-25 22:20:08 -0500

    CCVO: CLEAR LIBRARYS

--- src/feature/core/component/promotion/promotion.tsx
+++ src/feature/core/component/promotion/promotion.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { SearchComponent } from '../searchComponent/searchComponent'
 import { useGetList } from '../../services/useGetList'
 
